3b5bc55291b26ddba8dee9430c0297e6ea657fc6
Eduardo Contreras Hernández <eduardo.contreras.isw@unipolidgo.edu.mx>
2024-09-27 02:26:16 -0600

    Pantalla Home

---
+++ app/src/main/java/com/example/samid/RegisterPatient.kt
@@ -0,0 +1,37 @@
+package com.example.samid
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterPatient : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register_patient)
+
+        // Configurar la vista para manejar los márgenes de las barras del sistema
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Configuración del Spinner (ComboBox)
+        val spinner = findViewById<Spinner>(R.id.spinnerCondition)
+
+        // Lista de opciones
+        val options = arrayOf("Stable", "Critical")
+
+        // Adaptador para el Spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Asignar el adaptador al Spinner
+        spinner.adapter = adapter
+    }
+}
